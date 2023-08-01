31f4191e03561290ef1d6fcd226f941c601c980e
kevnnard <londonokevin9@gmail.com>
2023-08-01 16:21:36 -0500

    FIX: menu header is ready to use add mega MENU, links icons

--- src/interfaces/Page/header.interface.tsx
+++ src/interfaces/Page/header.interface.tsx
@@ -11,11 +11,45 @@ interface LinkMenuInterface {
    */
   link: string;
   /**
+   * REQUIRED: title for the link
+   */
+  titleMenu: string;
+  /**
    * REQUIRED: name from file for icon menu link in the navigator
    */
   iconMenu: string;
   /**
-   * REQUIRED: title for the link
+   * OPCIONAL: list of link children for mega menu
    */
-  titleMenu: string;
+  megaMenu?: Array<any>;
+}
+/**
+ * Interface for megaMenu links
+ */
+interface LinkMegaMenu {
+  /**
+   * REQUIRED: name of item link in mega menu children
+   */
+  itemTitle: string;
+  /**
+   * REQUIRED: list of ityem links to category
+   */
+  itemLinks: Array<ItemLink>;
+}
+/**
+ * Interfface for item link in item mega menu
+ */
+interface ItemLink {
+  /**
+   * REQUIRED: url for item link
+   */
+  link: string;
+  /**
+   * REQUIRED: name for link in item link
+   */
+  name: string;
+  /**
+   * OPCIONAL: icon image etc, for item link
+   */
+  icon?: string;
 }
